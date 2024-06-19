2170d7973bb9647b5656021b0d3144b3e5c8bf73
Amir <haghighi251@gmail.com>
2024-06-19 20:18:45 +0200

    feat: add DomainExceptionFilter to the main module

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { DomainExceptionFilter } from '@contexts/shared/domain/domain-exception.filter';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new DomainExceptionFilter());
 
   console.log(`🚀 Service ready at port: ${port}`);
   await app.listen(port);
